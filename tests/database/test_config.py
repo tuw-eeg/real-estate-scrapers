db0f02672f8cf1e3f8318a031713f6b87b18aae3
Péter Ferenc Gyarmati <dev.petergy@gmail.com>
2022-04-20 21:00:22 +0200

    test: test env-based db config

--- tests/database/test_config.py
+++ tests/database/test_config.py
@@ -1,17 +1,49 @@
 """Database config tests."""
 import re
+from os import environ
 
 from real_estate_scrapers.database.config import get_database_url, get_db_config
 
 
-def test_config_loads() -> None:
+def test_config_loads_from_file() -> None:
     assert get_db_config() is not None
+
+
+def test_config_loads_from_env() -> None:
+    required_env_vars = (
+        "POSTGRES_DB",
+        "POSTGRES_HOST",
+        "POSTGRES_PORT",
+        "POSTGRES_USER",
+        "POSTGRES_PASSWORD",
+    )
+    values = ("test", "localhost", "5432", "test", "test")
+    # Setup
+    for key, value in zip(required_env_vars, values):
+        environ[key] = value
+
+    # Test
+    db_config = get_db_config()
+    assert db_config is not None
+
+    url = get_database_url()
+    assert url is not None
+    assert _db_url_is_valid(url)
+    assert url == "postgresql://test:test@localhost:5432/test"
+
+    # Teardown
+    for key in required_env_vars:
+        del environ[key]
 
 
 def test_database_url_is_valid() -> None:
     url = get_database_url()
     assert url is not None
-    assert (
+    assert _db_url_is_valid(url)
+
+
+def _db_url_is_valid(url: str) -> bool:
+    return (
         re.match(r"^postgresql://[a-zA-Z0-9_\-]+:[a-zA-Z0-9_\-]+@[a-zA-Z0-9_\-\.]+:[0-9]+/[a-zA-Z0-9_\-]+$", url)
         is not None
     )
